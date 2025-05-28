59d226ea5caf8f4a15b7dffbe4302ae6bb93cbf7
ITNIKITAIT <nikito4ka281205@gmail.com>
2025-05-29 00:38:30 +0300

    add evaluate function

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,11 @@
-// Exceptions
-
-class UnknownOperatorException {
-    symbol: string;
-
-    constructor(symbol: string) {
-        this.symbol = symbol;
-    }
-}
-
-class UnmatchedOpeningBracketException {
-}
-
-class UnmatchedClosingBracketException {
-}
-
-
-// Types
-
-type Code = Operator[];
-
-type Operator =
-    | { kind: "Loop", code: Code }
-    | { kind: "Add", value: number }
-    | { kind: "Move", value: number }
-    | { kind: "Input" }
-    | { kind: "Output" };
-
-// Functions
+import { evaluateValue } from './evaluateValue';
+import {
+    Code,
+    Operator,
+    UnknownOperatorException,
+    UnmatchedClosingBracketException,
+    UnmatchedOpeningBracketException,
+} from './types';
 
 function opsMatch(a: Operator, b: Operator): boolean {
     return a.kind == b.kind;
@@ -34,34 +13,34 @@ function opsMatch(a: Operator, b: Operator): boolean {
 
 function symbolToOp(symbol: string): Operator {
     switch (symbol) {
-        case "+":
-            return {kind: "Add", value: 1};
-        case "-":
-            return {kind: "Add", value: -1};
-        case ">":
-            return {kind: "Move", value: 1};
-        case "<":
-            return {kind: "Move", value: -1};
-        case ",":
-            return {kind: "Input"};
-        case ".":
-            return {kind: "Output"};
+        case '+':
+            return { kind: 'Add', value: 1 };
+        case '-':
+            return { kind: 'Add', value: -1 };
+        case '>':
+            return { kind: 'Move', value: 1 };
+        case '<':
+            return { kind: 'Move', value: -1 };
+        case ',':
+            return { kind: 'Input' };
+        case '.':
+            return { kind: 'Output' };
         default:
             throw new UnknownOperatorException(symbol);
     }
 }
 
-function printCode(code: Code, tabulation: number = 0) {
-    for(const op of code) {
-        if (op.kind != "Loop") {
-            let s = JSON.stringify(op)
-            for (let i = 0; i < tabulation; ++i) s = " " + s
-            console.log(s)
-        } else {
-            printCode(op.code, tabulation + 4);
-        }
-    }
-}
+// function printCode(code: Code, tabulation: number = 0) {
+//     for (const op of code) {
+//         if (op.kind != 'Loop') {
+//             let s = JSON.stringify(op);
+//             for (let i = 0; i < tabulation; ++i) s = ' ' + s;
+//             console.log(s);
+//         } else {
+//             printCode(op.code, tabulation + 4);
+//         }
+//     }
+// }
 
 function parse(s: string): Code {
     const result: Code = [];
@@ -72,16 +51,18 @@ function parse(s: string): Code {
             s = s.substring(1);
 
             switch (symbol) {
-                case "+":
-                case "-":
-                case "<":
-                case ">":
+                case '+':
+                case '-':
+                case '<':
+                case '>':
                     let op = symbolToOp(symbol);
-                    let lastOp = result.at(-1)
+                    let lastOp = result.at(-1);
 
                     if (lastOp && opsMatch(op, lastOp)) {
-                        if (lastOp.kind != "Add" && lastOp.kind != "Move") throw new Error("Unreachable");
-                        if (op.kind != "Add" && op.kind != "Move") throw new Error("Unreachable");
+                        if (lastOp.kind != 'Add' && lastOp.kind != 'Move')
+                            throw new Error('Unreachable');
+                        if (op.kind != 'Add' && op.kind != 'Move')
+                            throw new Error('Unreachable');
 
                         lastOp.value += op.value;
                     } else {
@@ -89,14 +70,14 @@ function parse(s: string): Code {
                     }
 
                     break;
-                case ",":
-                case ".":
+                case ',':
+                case '.':
                     result.push(symbolToOp(symbol));
                     break;
 
-                case "[":
+                case '[':
                     let caught = false;
-                    const loopCode: Code = []
+                    const loopCode: Code = [];
 
                     try {
                         innerHelper(loopCode);
@@ -109,19 +90,18 @@ function parse(s: string): Code {
                     }
 
                     if (!caught) {
-                        throw new UnmatchedOpeningBracketException()
+                        throw new UnmatchedOpeningBracketException();
                     }
 
-                    result.push({ kind: "Loop", code: loopCode });
+                    result.push({ kind: 'Loop', code: loopCode });
                     break;
 
-                case "]":
-                    throw new UnmatchedClosingBracketException()
+                case ']':
+                    throw new UnmatchedClosingBracketException();
                 default:
                     break;
             }
         }
-
     }
 
     innerHelper(result);
@@ -130,7 +110,10 @@ function parse(s: string): Code {
 }
 
 function main() {
-    printCode(parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++."))
+    const parsed = parse(
+        '++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.'
+    );
+    console.log(evaluateValue(parsed));
 }
 
-main()
+main();
